bb90bce656b508a97c4b7b6efc76379848fc589a
lab <lab@mbqd.phy.cam.ac.uk>
2016-08-24 14:13:33 +0100

    cleaned up directory

---
+++ imageanalyzer.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Aug 24 14:01:51 2016
+
+@author: Konrad
+
+This script searches the currently active sequence directory for new images.
+"""
+import numpy as np
+import matplotlib.pyplot as plt
+
